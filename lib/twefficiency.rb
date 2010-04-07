074de05ba88c53b14a7b9ae1ccc8501eeb5cd564
Tammer Saleh <tsaleh@gmail.com>
2010-04-06 18:04:45 -0700

    wtf

--- lib/twefficiency.rb
+++ lib/twefficiency.rb
@@ -1,47 +1,45 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/ratpack'
 require 'helpers'
 require 'haml'
 require 'sass'
 require 'twitter'
 
-class Twefficiency < Sinatra::Base
-  root_dir = File.join(File.dirname(__FILE__), "..")
-  env = ENV['RACK_ENV'].to_sym
+root_dir = File.join(File.dirname(__FILE__), "..")
+set :root, root_dir
 
-  set :root,        root_dir
-  set :environment, env
-  set :public,      File.expand_path(root_dir + '/public')
+# FileUtils.mkdir_p(File.join(root_dir, "log")) unless File.exists?('log')
+# log = File.new(File.join(root_dir, "log", "sinatra.log"), "a")
+# $stdout.reopen(log)
+# $stderr.reopen(log)
 
-  helpers Sinatra::Ratpack
-  helpers Helpers
+helpers Sinatra::Ratpack
+helpers Helpers
 
-  get "/" do
-    haml :index
-  end
-
-  get '/for' do
-    @username = params[:username]
-    @tweets = Twitter::Search.new.from(@username).to_a
-    @total_chars = @tweets.inject(0) {|sum, tweet| sum += tweet.text.length }
-    if @tweets.empty?
-      @avg_tweet_length = 0
-      @twefficiency = 0
-    else
-      @avg_tweet_length = @total_chars / @tweets.length unless @tweets.length.zero?
-      @twefficiency = @avg_tweet_length / 140.0
-    end
-    haml :results
-  end
-
-  get '/about' do
-    haml :about
-  end
-
-  get '/stylesheets/sass/:sheet.css' do
-    content_type 'text/css', :charset => 'utf-8'
-    sass :"stylesheets/#{params[:sheet]}"
-  end
-  
+get "/" do
+  haml :index
 end
 
+get '/for' do
+  @username = params[:username]
+  @tweets = Twitter::Search.new.from(@username).to_a
+  @total_chars = @tweets.inject(0) {|sum, tweet| sum += tweet.text.length }
+  if @tweets.empty?
+    @avg_tweet_length = 0
+    @twefficiency = 0
+  else
+    @avg_tweet_length = @total_chars / @tweets.length unless @tweets.length.zero?
+    @twefficiency = @avg_tweet_length / 140.0
+  end
+  haml :results
+end
+
+get '/about' do
+  haml :about
+end
+
+get '/stylesheets/sass/:sheet.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"stylesheets/#{params[:sheet]}"
+end
+  
